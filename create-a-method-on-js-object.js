f8a8b5230b2aaa74aba67e9011848fe0244b6115
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2022-12-13 10:18:55 +0000

    Create JS file

---
+++ create-a-method-on-js-object.js
@@ -0,0 +1,7 @@
+let dog = {
+  name: "Kerouac",
+  numLegs: 4,
+  sayName: function() {return "His name is " + dog.name + " as the American author!"}
+  };
+  
+  console.log(dog.sayName()); --> it prints to the screen the sentence "His name is Kerouac as the American author!" 
